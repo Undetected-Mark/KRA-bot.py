889497c2258affbd76efd13f69293130f5e95129
BAD-PI <149628810+BAD-PI-cybsec@users.noreply.github.com>
2024-03-21 21:18:42 +0200

    V 2.0

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+# Define intents
+intents = discord.Intents.default()
+intents.messages = True  # Enable message-related intents
+
+# Create a bot instance with a specified command prefix and intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Event to print bot ready message when it's fully loaded
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+# Event to handle messages
+@bot.event
+async def on_message(message):
+    print(f"Received message: {message.content}")
+    # Echo back the message
+    await message.channel.send(message.content)
+
+    # Let the command processing continue
+    await bot.process_commands(message)
+# Run the bot with its token
+bot.run('MTIwNjI5NTY1MTY2MzYxODA3OQ.Gd17nv.SPMQEhMw5spuHI7_msJrqI3v4xCH69Q-kSBDOc')
+
+
+#!python3 /home/bad-pi/Code/Discord_Bot/main.py
